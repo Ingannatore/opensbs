fecdb046ef4136977799102606ac69d8d40f4eb9
Ingannatore <matteo.guindani@gmail.com>
2019-08-29 18:51:58 +0200

    Renamed client folder, fixed vulnerabilities

--- src/OpenSBS.Server/Startup.cs
+++ src/OpenSBS.Server/Startup.cs
@@ -25,7 +25,7 @@ namespace OpenSBS.Server
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/build";
+                configuration.RootPath = "client/build";
             });
         }
 
@@ -56,7 +56,7 @@ namespace OpenSBS.Server
 
             app.UseSpa(spa =>
             {
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "client";
 
                 if (env.IsDevelopment())
                 {
--- src/OpenSBS.Server/client/src/components/Home.js
+++ src/OpenSBS.Server/client/src/components/Home.js
@@ -16,7 +16,7 @@ const Home = props => (
       <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
       <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
     </ul>
-    <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+    <p>The <code>client</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
   </div>
 );
 
