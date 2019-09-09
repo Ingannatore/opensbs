d0b959ba30188e6872431eb9820721ade479d309
Ingannatore <matteo.guindani@gmail.com>
2019-09-09 21:14:11 +0200

    Added new rudder (experimental)

--- src/OpenSBS.Server/client/src/svg/stations/helm.js
+++ src/OpenSBS.Server/client/src/svg/stations/helm.js
@@ -1,12 +1,25 @@
 ﻿import React, {Component} from 'react'
 import Station from "./index";
 import Compass from "../widgets/compass";
+import Rudder from "../widgets/rudder";
 
 class Helm extends Component {
     render() {
         return (
             <Station id="helm">
                 <Compass x="240" y="240"/>
+                <Rudder x="240" y="760"/>
+
+                <line x1="0" y1="1020" x2="1920" y2="1020" stroke="#36424a" strokeWidth="1"/>
+                <rect x="0" y="1021" width="1920" height="79" fill="#070d0f" opacity=".75" />
+
+                <text x="760" y="1055" textAnchor="middle" fontSize="2rem" fill="#76797c">WRP</text>
+                <text x="960" y="1055" textAnchor="middle" fontSize="2rem" fill="white">NAV</text>
+                <text x="1160" y="1055" textAnchor="middle" fontSize="2rem" fill="#76797c">STA</text>
+
+                <g transform="translate(960, 1025) rotate(180)">
+                    <path d="M 0 -4 L 4 4 L -4 4 Z" stroke="#36424a" strokeWidth="1" fill="#36424a"/>
+                </g>
             </Station>
         );
     }
---
+++ src/OpenSBS.Server/client/src/svg/widgets/rudder.js
@@ -0,0 +1,88 @@
+﻿import React, {Component} from 'react'
+import {connect} from "react-redux";
+import Container from "../elements/container";
+import Actions from "../../actions";
+
+class Rudder extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { follow: false, px: 0, py: 0 };
+        this.mouseDownHandler = this.mouseDownHandler.bind(this);
+        this.mouseMoveHandler = this.mouseMoveHandler.bind(this);
+        this.mouseUpHandler = this.mouseUpHandler.bind(this);
+    }
+
+    render() {
+        const translateRudder = `translate(${this.state.px}, ${this.state.py})`;
+        return (
+            <g onMouseDown={ this.mouseDownHandler } onMouseMove={ this.mouseMoveHandler } onMouseUp={ this.mouseUpHandler }>
+                <Container id="widget-rudder" size="220" x={this.props.x} y={this.props.y}>
+                    <line x1="0" y1="215" x2="0" y2="-215" stroke="#33393d" strokeWidth="1" strokeDasharray="2 4" />
+                    <line x1="0" y1="215" x2="0" y2="-215" stroke="#33393d" strokeWidth="1" transform="rotate(45)" strokeDasharray="2 4" />
+                    <line x1="0" y1="215" x2="0" y2="-215" stroke="#33393d" strokeWidth="1" transform="rotate(90)" strokeDasharray="2 4" />
+                    <line x1="0" y1="215" x2="0" y2="-215" stroke="#33393d" strokeWidth="1" transform="rotate(135)" strokeDasharray="2 4" />
+
+                    <circle cx="0" cy="0" r="60" stroke="none" fill="black" opacity=".4"/>
+                    <g transform={translateRudder}>
+                        <circle cx="0" cy="0" r="60" stroke="#33393d" strokeWidth="2" fill="#122127" />
+                        <circle cx="0" cy="0" r="56" stroke="#85888a" fill="none" />
+                        <text x="0" y="0" textAnchor="middle" fontSize="1.4rem" fill="#ffffff">rudder</text>
+                    </g>
+                </Container>
+            </g>
+        );
+    }
+
+    mouseDownHandler(event) {
+        this.getRudderPosition(event);
+        this.setState({
+            follow: true
+        });
+    }
+
+    mouseMoveHandler(event) {
+        if (!this.state.follow) {
+            return;
+        }
+
+        this.getRudderPosition(event);
+    }
+
+    mouseUpHandler() {
+        this.setState({follow: false, px: 0, py: 0});
+        this.props.dispatch(Actions.updateState('ship.rudder', 0))
+    }
+
+    getRudderPosition(event) {
+        const angle = Math.atan2(
+            event.clientY - this.props.y,
+            event.clientX - this.props.x
+        );
+
+        const px = 150 * Math.cos(angle);
+        this.dispatchUpdateState(px);
+
+        this.setState({
+            px: px,
+            py: 150 * Math.sin(angle)
+        });
+    }
+
+    dispatchUpdateState(px) {
+        if (px > 0) {
+            this.props.dispatch(Actions.updateState('ship.rudder', +1))
+        }
+        else if (px < 0) {
+            this.props.dispatch(Actions.updateState('ship.rudder', -1))
+        }
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        rudder: state['ship.rudder'] || '0'
+    }
+};
+
+export default connect(mapStateToProps)(Rudder);
