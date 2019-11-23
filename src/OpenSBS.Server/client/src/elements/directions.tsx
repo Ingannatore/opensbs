beca2b263571bfd90c91e3402477da91f4db83a5
Ingannatore <matteo.guindani@gmail.com>
2019-11-23 17:05:07 +0100

    Refactored radar sub-components

---
+++ src/OpenSBS.Server/client/src/elements/directions.tsx
@@ -0,0 +1,48 @@
+﻿import * as React from 'react';
+
+interface DirectionsComponentProps {
+    x: number,
+    y: number,
+    size: number,
+    angles: string
+}
+
+export default class Directions extends React.Component<DirectionsComponentProps, {}> {
+    private readonly translation: string;
+
+    public static defaultProps = {
+        x: 0,
+        y: 0,
+        angles: '0 45 90 135'
+    };
+
+    constructor(props: DirectionsComponentProps) {
+        super(props);
+        this.translation = this.props.x || this.props.y ? `translate(${this.props.x} ${this.props.y})` : '';
+    }
+
+    public render() {
+        const lines = this.props.angles
+            .split(' ')
+            .map((value: string) => parseInt(value))
+            .map((angle: number) => Directions.renderLine(this.props.size, angle));
+
+        return (
+            <g transform={this.translation}>
+                {lines}
+            </g>
+        );
+    }
+
+    private static renderLine(size: number, angle: number) {
+        const transform = angle ? `rotate(${angle})` : '';
+        return (
+            <line
+                x1="0" y1={-size}
+                x2="0" y2={size}
+                stroke="#2a363c" strokeDasharray="2 4"
+                transform={transform}
+            />
+        );
+    }
+}
--- src/OpenSBS.Server/client/src/widgets/radar/index.tsx
+++ src/OpenSBS.Server/client/src/widgets/radar/index.tsx
@@ -8,6 +8,8 @@ import Button from "../../elements/button";
 import Actions from "./actions";
 import {RadarState} from "./state";
 import Display from "../../elements/display";
+import Directions from "../../elements/directions";
+import Ranges from "../../elements/ranges";
 
 interface RadarComponentProps {
     dispatch: any,
@@ -38,31 +40,11 @@ class Radar extends React.Component<RadarComponentProps, {}> {
                 <Bezel size={450} stroke={'none'} rotation={this.props.rotation.y} interval={3} majorInterval={15} fontSize={1}/>
 
                 {this.props.radar.enableDirectionLines && (
-                    <g>
-                        <line x1="0" y1="-440" x2="0" y2="440" stroke="#2a363c" strokeWidth="1" strokeDasharray="2 4"/>
-                        <line x1="0" y1="-440" x2="0" y2="440" stroke="#2a363c" strokeWidth="1" strokeDasharray="2 4" transform="rotate(45)"/>
-                        <line x1="0" y1="-440" x2="0" y2="440" stroke="#2a363c" strokeWidth="1" strokeDasharray="2 4" transform="rotate(90)"/>
-                        <line x1="0" y1="-440" x2="0" y2="440" stroke="#2a363c" strokeWidth="1" strokeDasharray="2 4" transform="rotate(135)"/>
-                    </g>
+                    <Directions size={440}/>
                 )}
 
                 {this.props.radar.enableRangeCircles && (
-                    <g>
-                        <circle cx="0" cy="0" r="88" stroke="#122127" fill="none" strokeWidth="1"/>
-                        <circle cx="0" cy="0" r="176" stroke="#122127" fill="none" strokeWidth="1"/>
-                        <circle cx="0" cy="0" r="264" stroke="#122127" fill="none" strokeWidth="1"/>
-                        <circle cx="0" cy="0" r="352" stroke="#122127" fill="none" strokeWidth="1"/>
-                    </g>
-                )}
-
-                {this.props.radar.enableRangeTexts && (
-                    <g>
-                        <text x="84" y="-8" fontSize="1rem" fill="#76797c" textAnchor="end">2km</text>
-                        <text x="172" y="-8" fontSize="1rem" fill="#76797c" textAnchor="end">4km</text>
-                        <text x="260" y="-8" fontSize="1rem" fill="#76797c" textAnchor="end">6km</text>
-                        <text x="348" y="-8" fontSize="1rem" fill="#76797c" textAnchor="end">8km</text>
-                        <text x="436" y="-8" fontSize="1rem" fill="#76797c" textAnchor="end">10km</text>
-                    </g>
+                    <Ranges size={440} distance={10000} showTextes={this.props.radar.enableRangeTexts}/>
                 )}
 
                 <circle cx="0" cy="0" r="440" stroke="#33393d" fill="none" strokeWidth="1"/>
