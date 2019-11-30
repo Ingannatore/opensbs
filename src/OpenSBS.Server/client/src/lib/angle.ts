1b65d4a3c72e3a9562d36b8908caef21ac72ecfb
Ingannatore <matteo.guindani@gmail.com>
2019-11-30 15:41:52 +0100

    Refactored models and libs

---
+++ src/OpenSBS.Server/client/src/lib/angle.ts
@@ -0,0 +1,9 @@
+﻿export default class Angle {
+    public static toDegrees(radians: number): number {
+        return radians * (180 / Math.PI);
+    }
+
+    public static toRadians(degrees: number): number {
+        return degrees * (Math.PI / 180);
+    }
+}
---
+++ src/OpenSBS.Server/client/src/lib/coords.ts
@@ -0,0 +1,11 @@
+﻿import Vector3 from '../models/vector3';
+
+export default class Coords {
+    public static toCarthesian(r: number, theta: number): Vector3 {
+        return {
+            x: r * Math.cos(theta),
+            y: r * Math.sin(theta),
+            z: 0
+        };
+    }
+}
