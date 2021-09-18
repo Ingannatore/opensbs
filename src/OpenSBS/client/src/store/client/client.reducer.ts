dd91eb9a282a3413b388a8bb04c7d9d80f6fba86
Ingannatore <matteo.guindani@gmail.com>
2021-09-18 21:18:58 +0200

    Fixed target selection when target is destroyed

--- src/OpenSBS/client/src/store/client/client.reducer.ts
+++ src/OpenSBS/client/src/store/client/client.reducer.ts
@@ -1,11 +1,25 @@
 ﻿import ActionModel from '../action.model';
 import ClientActions from './client.actions';
 import ClientStateModel from './client-state.model';
+import {EntityModel} from '../../models/entity.model';
+import EntityTraceModel from '../../modules/entity-trace.model';
+import SpaceshipActions from '../spaceship/spaceship.actions';
+import {EntityModuleModel} from '../../models/entity-module.model';
+import {SensorsModuleModel} from '../../modules/sensors-module.model';
+import ModuleType from '../../modules/module-type';
 
 const defaultState: ClientStateModel = {
     selectedTarget: null,
     selectedAmmo: null,
 };
+
+const existsTrace = (entity: EntityModel, trace: EntityTraceModel): boolean => {
+    const sensors = entity.modules.find(
+        (module: Partial<EntityModuleModel>) => module.type === ModuleType.SENSORS
+    ) as SensorsModuleModel;
+
+    return sensors.traces.find(it => it.id === trace.id) != undefined;
+}
 
 export default (state = defaultState, action: ActionModel) => {
     if (action.type === ClientActions.Types.SET_TARGET && action.payload) {
@@ -22,6 +36,12 @@ export default (state = defaultState, action: ActionModel) => {
 
     if (action.type === ClientActions.Types.RESET_AMMO) {
         return {...state, selectedAmmo: null};
+    }
+
+    if (state.selectedTarget && action.type === SpaceshipActions.Types.REFRESH && action.payload) {
+        if (!existsTrace(JSON.parse(action.payload), state.selectedTarget)) {
+            return {...state, selectedTarget: null};
+        }
     }
 
     return state;
