58170a3c413d2f09d552528b071430f5b006710c
Ingannatore <matteo.guindani@gmail.com>
2023-10-08 17:33:19 +0200

    Implemented waypoints management

--- src/OpenSBS.Core/Components/ElectronicComponent.cs
+++ src/OpenSBS.Core/Components/ElectronicComponent.cs
@@ -1,26 +1,43 @@
 ﻿using OpenSBS.Core.Models;
+using System.Numerics;
 
 namespace OpenSBS.Core.Components
 {
     internal class ElectronicComponent
     {
+        private const string AddWaypointAction = "waypoint.add";
+        private const string RemoveWaypointAction = "waypoint.remove";
+
         public int SensorsRange { get; }
         public EntityTraceCollection Traces { get; private set; }
+        public IList<Waypoint> Waypoints { get; private set; }
 
         public ElectronicComponent(int sensorsRange)
         {
             SensorsRange = sensorsRange;
             Traces = new();
+            Waypoints = new List<Waypoint>();
         }
 
         public void HandleCommand(ComponentCommand command)
         {
-            throw new NotImplementedException();
+            switch (command.Action)
+            {
+                case AddWaypointAction:
+                    Waypoints.Add(new(command.PayloadTo<Vector2>()));
+                    break;
+
+                case RemoveWaypointAction:
+                    var index = command.PayloadTo<int>();
+                    if (index < Waypoints.Count) Waypoints.RemoveAt(index);
+                    break;
+            }
         }
 
         public void Update(TimeSpan deltaT, Entity owner, World world)
-        {            
+        {
             UpdateTraces(owner, world);
+            UpdateWaypoints(owner);
         }
 
         private void UpdateTraces(Entity owner, World world)
@@ -36,7 +53,20 @@ namespace OpenSBS.Core.Components
                     continue;
                 }
 
-                Traces.GetOrCreateTrace(entity.Id).Update(owner.GetBearingTo(entity.Body.Position), distance);
+                Traces
+                    .GetOrCreateTrace(entity.Id)
+                    .Update(owner.GetBearingTo(entity.Body.Position), distance);
+            }
+        }
+
+        private void UpdateWaypoints(Entity owner)
+        {
+            foreach (var waypoint in Waypoints)
+            {
+                waypoint.Update(
+                    owner.GetBearingTo(waypoint.Position),
+                    owner.GetDistanceTo(waypoint.Position)
+                );
             }
         }
     }
---
+++ src/OpenSBS.Core/Models/Waypoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace OpenSBS.Core.Models
+{
+    internal class Waypoint
+    {
+        public Vector2 Position { get; }
+        public int Bearing { get; protected set; }
+        public float Distance { get; protected set; }
+
+        public Waypoint(Vector2 position)
+        {
+            Position = position;
+        }
+
+        public void Update(int bearing, float distance)
+        {
+            Bearing = bearing;
+            Distance = distance;
+        }
+    }
+}
